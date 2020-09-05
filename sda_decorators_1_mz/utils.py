214fe2a8aa82d3b60b6d7c0442d57fc888be4dbc
Adrian <a.staszewski93@gmail.com>
2020-09-05 11:38:29 +0200

    decoratos accomplishment

--- sda_decorators_1_mz/utils.py
+++ sda_decorators_1_mz/utils.py
@@ -18,27 +18,29 @@ def very_simply_wrapper(func):
 
 def my_wrapper(func):
     def wrapper(*args, **kwargs):
-        print("before")
+        #   def wrapper():
+        print('before')
         print(f'args = {args}')
         print(f'kwargs = {kwargs}')
         result = func(*args, **kwargs)
-        print('after')
+        #       result = func()
+        print(' after')
         return result
 
-    return wrapper()
+    return wrapper
 
 
 # @simply_wrapper
 @my_wrapper
-def say_hello(a=7, b=3):
-    print("Hell World!")
-    print(f'a value = {a}')
-    print(f'b value = {b}')
+def say_hello(a, b):
+    print('hello world!')
+    print(f" a value ={a}")
+    print(f" b value = {b}")
 
 
 def main():
-    say_hello(1, 2)
+    say_hello(a=1, b=2)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
